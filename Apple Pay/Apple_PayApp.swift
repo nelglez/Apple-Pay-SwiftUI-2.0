e52e7341d6960c9c6e7f539a581b5651e50448af
Nelson Gonzalez <nelglezfl@gmail.com>
2020-09-20 07:33:15 -0400

    Initial Commit

---
+++ Apple Pay/Apple_PayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_PayApp.swift
+//  Apple Pay
+//
+//  Created by Nelson Gonzalez on 9/20/20.
+//
+
+import SwiftUI
+
+@main
+struct Apple_PayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Pay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple Pay
+//
+//  Created by Nelson Gonzalez on 9/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
